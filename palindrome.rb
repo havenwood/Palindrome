a450ffca03b1f54004ce81768aa1557eae024149
Havenwood <shannonskipper@gmail.com>
2011-10-14 14:27:54 -0700

    green light

--- palindrome.rb
+++ palindrome.rb
@@ -1,19 +1,19 @@
 #!/usr/bin/env ruby
 
 module FindPalindromes
-  def find_even_palindromes
-    while @array[looking_here_before] == @array[looking_forward]
-      unless looking_here_before < 0 || @reach == 1
-        @palindromes << @array[looking_here_before, @reach * 2].join
-      end
-      @reach += 1
-    end
-  end
-
   def find_odd_palindromes
     while @array[looking_back] == @array[looking_forward]
       unless looking_back < 0
         @palindromes << @array[looking_back, @reach * 2 + 1].join
+      end
+      @reach += 1
+    end
+  end
+  
+  def find_even_palindromes
+    while @array[looking_here_before] == @array[looking_forward]
+      unless looking_here_before < 0 || @reach == 1
+        @palindromes << @array[looking_here_before, @reach * 2].join
       end
       @reach += 1
     end
@@ -45,7 +45,7 @@ class PalindromeFinder
     private
   
     def initialize_palindromes string
-      @array = string.to_s.downcase.gsub(/[^0-9a-z]/, '').split ''
+      @array = string.to_s.downcase.gsub(/[^0-9a-z]/, '').each_char.to_a
       @palindromes = []
     end
     
@@ -56,8 +56,8 @@ class PalindromeFinder
       @array.size.times do |spot|
         @spot = spot
         @reach = 1
-        find_even_palindromes
-        find_odd_palindromes      
+        find_odd_palindromes 
+        find_even_palindromes     
       end
     end
         
